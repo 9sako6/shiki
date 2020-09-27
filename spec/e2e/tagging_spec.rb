6d450391d87575e681ac58aae52077b696e37cec
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-09-27 19:18:30 +0900

    Add video validation (allows only MP4)

--- spec/e2e/tagging_spec.rb
+++ spec/e2e/tagging_spec.rb
@@ -3,11 +3,7 @@ require 'rails_helper'
 feature 'tagging' do
   before do
     @video = create(:video)
-    @video.video = fixture_file_upload('./spec/fixtures/videos/mouse.mov', 'video/mov')
-    @video.save!
-
     @tag = create(:tag)
-    @tag.save!
   end
 
   scenario 'should add a tag to a video' do
--- spec/factories/video.rb
+++ spec/factories/video.rb
@@ -1,5 +1,8 @@
+include ActionDispatch::TestProcess
+
 FactoryBot.define do
   factory :video do
     title { 'hamster.mp4' }
+    video { fixture_file_upload('spec/fixtures/videos/mouse.mp4') }
   end
 end
